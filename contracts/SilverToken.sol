e77bea3d34650426bc01335ee00be64fddda6337
BTC415 <alenschmitt415@gmail.com>
2024-10-30 01:05:44 +0000

    [add] added several functions for silverToken contract

--- contracts/SilverToken.sol
+++ contracts/SilverToken.sol
@@ -49,6 +49,9 @@ contract SilverToken is ReentrancyGuard {
     ///@dev address of contract owner
     address public owner;
 
+    /// @dev ICO creator
+    address public creator;
+
     ///@dev DAO address
     address public daoAddress;
 
@@ -66,6 +69,9 @@ contract SilverToken is ReentrancyGuard {
 
     ///@dev hardcap of ICO
     uint256 public hardcap;
+
+    ////@dev funds raised
+    uint256 public fundsRaised;
 
     ///@dev projectURI data
     string public projectURI;
@@ -180,8 +186,9 @@ contract SilverToken is ReentrancyGuard {
         address ico,
         address distributor,
         uint256 fundsRaised,
-        uint256 listerFee,
         uint256 daoFee,
+        uint256 listerFee,
+        uint256 creatorFee,
         uint256 timestamp
     );
 
@@ -205,6 +212,7 @@ contract SilverToken is ReentrancyGuard {
      *@param   totalSupply_ token totalSupply
      *@param   decimal_ token decimal
      *@param   price_ token price
+     *@param   creator_ address of creator
      *@param   tokenAddress_ token tokenAdress
      *@param   listerAddress_ address of listing partner
      *@param   daoAddress_ address of DAO owner
@@ -220,6 +228,7 @@ contract SilverToken is ReentrancyGuard {
         uint256 totalSupply_,
         uint256 decimal_,
         uint256 price_,
+        address creator_,
         address tokenAddress_,
         address listerAddress_,
         address daoAddress_,
@@ -231,6 +240,7 @@ contract SilverToken is ReentrancyGuard {
         totalSupplyAbleToReachHardcap(totalSupply_, price_, decimal_, hardcap_)
         notZeroAmount(totalSupply_)
         notZeroAmount(decimal_)
+        notZeroAdderss(creator_)
         notZeroAdderss(tokenAddress_)
         notZeroAdderss(listerAddress_)
         notZeroAdderss(daoAddress_)
@@ -248,6 +258,7 @@ contract SilverToken is ReentrancyGuard {
         tokenInfo.price = price_;
         tokenInfo.tokenAddress = tokenAddress_;
 
+        creator = creator_;
         lister = listerAddress_;
         daoAddress = daoAddress_;
         fundRaisedAddress = fundRaisedAddress_;
@@ -331,4 +342,200 @@ contract SilverToken is ReentrancyGuard {
     function totalCap() public view returns (uint256) {
         return hardcap / tokenInfo.price;
     }
+
+    /**
+     * @dev buy tokens using ETH
+     * @param amount_ ETH amount to invest
+     * @param contributor_ contribution partner's address
+     */
+    function invest(
+        uint amount_,
+        address contributor_
+    ) external payable nonReentrant tokensChargedFully ableToBuy(amount_) {
+        require(block.timestamp < endTime, "ICO is ended");
+        require(amount_ > 0, "Invalid amount");
+        require(msg.value >= amount_, "Insufficient Eth amount");
+        require(contributor_ != address(0), "Invalid contributor's address");
+
+        if (investments[msg.sender] == 0) investors.push(msg.sender);
+        investments[msg.sender] += amount_;
+
+        if (contributions[contributor_] == 0) contributors.push(contributor_);
+        contributions[contributor_] += amount_;
+
+        uint256 _gap = msg.value - amount_;
+        if (_gap > 0) {
+            payable(msg.sender).transfer(_gap); // If there is any ETH left after purchasing tokens, it will be refunded.
+        }
+
+        // save investment history
+        history.push(
+            HISTORY(msg.sender, contributor_, amount_, block.timestamp)
+        );
+
+        fundsRaised += amount_;
+        if (fundsRaised >= hardcap) {
+            // Once the funds raised reach the hard cap, the ICO is completed and the funds are distributed.
+            endTime = block.timestamp - 1;
+            distribute();
+        }
+        emit Invest(
+            address(this),
+            msg.sender,
+            contributor_,
+            amount_,
+            block.timestamp
+        );
+    }
+
+    /**
+     * @dev when time is reach, creator finish ico
+     */
+    function finish() external payable nonReentrant {
+        require(block.timestamp > endTime, "ICO not ended yet.");
+
+        if (fundsRaised >= softcap) {
+            distribute(); // If funds raised reach softcap, distribute funds
+        } else {
+            finishNotSuccess(); // If the funds don't reach softcap, all investments will be refunded to investors
+        }
+    }
+
+    /**
+     * @dev If the ICO fails to reach the soft cap before the end of the self-set time, all funds will be refunded to investors.
+     */
+    function finishNotSuccess() internal {
+        // refunds all funds to investors
+        for (uint256 i = 0; i < investors.length; i++) {
+            address to = investors[i];
+            uint256 _amount = investments[to];
+            investments[to] = 0;
+            if (_amount > 0) payable(to).transfer(_amount);
+        }
+
+        // refunds all tokens to creator
+        uint256 _tokens = tokensAvailable();
+        SafeERC20.safeTransfer(silverToken, creator, _tokens);
+
+        // set refund information
+        refund.refunded = true;
+        refund.refunder = msg.sender;
+        refund.timestamp = block.timestamp;
+
+        emit FundsAllRefund(address(this), msg.sender, block.timestamp);
+    }
+
+    /**
+     * @dev Distribute fees to dao and partners and send funds to creators' wallets, and send tokens to investors.
+     */
+    function distribute() internal {
+        bool success = false;
+        // funds raised
+        uint256 _funds = fundsRaised;
+        // cryptoSI DADAO fee 2.5%
+        uint256 _daoFee = (_funds * 25) / 1000;
+        (success, ) = payable(daoAddress).call{value: _daoFee}("");
+        require(success, "Failed to send DAO fee.");
+        //listing partner's fee 1%
+        uint256 _listerFee = (_funds * 10) / 1000;
+        (success, ) = payable(lister).call{value: _listerFee}("");
+        require(success, "Failed to send listing partner's fee.");
+        //creator's funds 95%
+        uint256 _creatorFee = (_funds * 95) / 100;
+        (success, ) = payable(fundRaisedAddress).call{value: _creatorFee}("");
+        require(success, "Failed to send creator's funds.");
+
+        // distribute investor's contribution fees to contribution partners
+        for (uint256 i = 0; i < contributors.length; i++) {
+            address _to = contributors[i];
+            uint256 _amount = (contributions[_to] * 15) / 1000;
+            // send 1.5% to contribution partner
+            (success, ) = payable(_to).call{value: _amount}("");
+            require(success, "Failed to send contribution partner's fee.");
+        }
+        // distribute tokens to investors
+        for (uint256 i = 0; i < investors.length; i++) {
+            address _to = investors[i];
+            uint256 _amount = investments[_to];
+            uint256 _tokens = (_amount * 10 ** tokenInfo.decimal) /
+                tokenInfo.price;
+            SafeERC20.safeTransfer(silverToken, _to, _tokens);
+        }
+        // set distribution information
+        distribution.distributed = true;
+        distribution.distributor = msg.sender;
+        distribution.timestamp = block.timestamp;
+
+        emit FeeDistributed(
+            address(this),
+            msg.sender,
+            _funds,
+            _daoFee,
+            _listerFee,
+            _creatorFee,
+            block.timestamp
+        );
+    }
+
+    /**
+     * @dev get current state of this ICO
+     */
+    function getICOState() public view returns (ICOState _state) {
+        if (block.timestamp < endTime) {
+            _state = ICOState.SUCCESS;
+        } else if (fundsRaised >= hardcap) {
+            _state = ICOState.REACHEDHARDCAP;
+        } else if (fundsRaised < softcap) {
+            _state = ICOState.FAILED;
+        } else {
+            _state = ICOState.REACHEDSOFTCAP;
+        }
+        return _state;
+    }
+
+    /**
+     * @dev get all investors
+     */
+    function getInvestors() public view returns (address[] memory) {
+        return investors;
+    }
+
+    /**
+     * @dev Get the investor's investment amount
+     */
+    function getInvestAmount(address from) public view returns (uint256) {
+        return investments[from];
+    }
+
+    /**
+     * @dev Get all contributors
+     */
+    function getContributors() public view returns (address[] memory) {
+        return contributors;
+    }
+
+    /**
+     * @dev Get contribution partner's fee
+     */
+    function getContributorAmount(address from) public view returns (uint256) {
+        return contributions[from];
+    }
+
+    /**
+     * @dev get all investment history
+     */
+    function getHistory() public view returns (HISTORY[] memory) {
+        return history;
+    }
+
+    function getTokenAmountForInvestor(
+        address from
+    ) public view returns (uint256) {
+        uint256 _amount = investments[from];
+        uint256 _tokens = (_amount * 10 ** tokenInfo.decimal) / tokenInfo.price;
+        return _tokens;
+    }
+
+    receive() external payable {}
+    fallback() external payable {}
 }
