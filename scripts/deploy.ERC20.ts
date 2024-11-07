1c062560ae8a83beed251eaf4c62b92ceded264d
LovelyBTC <alenschmitt415@gmail.com>
2024-11-07 00:03:36 -0600

    [add] wrote deploy script

---
+++ scripts/deploy.ERC20.ts
@@ -0,0 +1,25 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  /////Early Liquidity
+  console.log("Deploying DAI token contracts with the account:", deployer.address);
+  const DAI = await ethers.getContractFactory("DAI");
+  const daiToken = await DAI.deploy(1000000000);
+  await daiToken.deployed();
+  console.log("DAI address:", await daiToken.getAddress());
+
+  console.log("Deploying DEW token contracts with the account:", deployer.address);
+  const DEW = await ethers.getContractFactory("DEW");
+  const dewToken = await DEW.deploy();
+  await dewToken.deployed();
+  console.log("DEW address:", await dewToken.getAddress());
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
